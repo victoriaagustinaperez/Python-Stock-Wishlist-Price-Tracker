f26759c85645cb54106e7dc1d29bcde7cb6100db
Victoria Pérez <45078724+victoriaagustinaperez@users.noreply.github.com>
2020-12-31 15:37:47 -0500

    changed comments

--- stock-price-tracker.py
+++ stock-price-tracker.py
@@ -5,7 +5,7 @@ from time import sleep
 #create list of symbols and split each value into individual symbol
 symbols = "AMZN GOOG NFLX FB GLD SPY".split()
 
-#personal stock portfolio (from Dec 2020) of stocks projected to grow between years 2020-2030
+#author's personal stock portfolio (compiled Dec 2020) of stocks projected to grow between years 2020-2030
 tech_symbols = "WUGI CEVA FIVG INSG AMD VUZI TSLA THNQ DT DLR BOTZ NIO INTC NVDA".split()
 www_symbols = "BIDU GOOG GOOGL".split()
 energy_symbols = "ALB ENPH SEDG BEP".split()
@@ -14,23 +14,23 @@ options = " Track all Lists, Show all Lists, \
 Add to Default List, Edit Default List, Search live price of any stock:, \
         Quit".split(",")
 
-#define function called get_prices which takes argument as split objects in list "symbols" and returns price
-def get_prices(symbols): #1
-    symbols.sort() #sorts in alphabetical order
+#define function called get_prices to take arguments as the split objects from list "symbols" and returns each object's price only
+def get_prices(symbols): #choice 1
+    symbols.sort() #sort in alphabetical order
     return pdr.get_quote_yahoo(symbols)['price'] 
         
-def show_default(symbols): #2
+def show_default(symbols): #choice 2
     symbols.sort() 
     return symbols
 
-def add_to_default(symbols): #3
+def add_to_default(symbols): #choice 3
     print("Enter symbol to add: ")
     symbol = input().upper() #convert input to uppercase
     while symbol != '':
         symbols.append(symbol)
         symbol = input("Hit enter at anytime to quit program.\nEnter symbol to add:") #hook
     
-def edit_default(symbols): #4
+def edit_default(symbols): #choice 4
     print("Select symbol to delete: ")
     #select from menu of symbols
     for i in range(1, len(symbols) + 1):
@@ -38,7 +38,7 @@ def edit_default(symbols): #4
     remove = symbols.pop(int(input())-1)
     print("{} removed".format(remove))
 
-def stock_search(): #5
+def stock_search(): #choice 5
     new_list = []
     print("Enter ticker symbol to search: ")
     symbol = input().upper()
@@ -51,12 +51,12 @@ def stock_search(): #5
 
 #define entry point for application
 def main():
-    run_program = True #hook feature to later change to False for Quit menu option
+    run_program = True
     while run_program: #infinite loop
         print("\nChoose Option:") #menu heading
         for i in range(1, len(options) + 1):
             print("{} - {}".format(i, options[i-1]))
-        #create checkpoint so not constantly running
+        #create checkpoint so loop not constantly running
         choice = int(input()) #user input integer
         
         #make a structure to implement certain code when a choice is made
